ac59ff74ceada00b3c5756f2c2f7cebfecea0847
bakyt1236 <apsamaov1@gmail.com>
2022-11-11 15:25:01 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=100;
+        int b=20;
+        int c= 10;
+        int h=a+b;
+        System.out.println(h);
+        h=h-40;
+        System.out.println(h);
+        int s=h*c;
+        System.out.println(s);
+        s=s/c;
+        System.out.println(s);
+    }
+}
